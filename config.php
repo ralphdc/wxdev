56fcd7152483a7f1678d219ed358ee29a5e87285
ralphdc <472298551@qq.com>
2018-05-01 13:54:36 +0800

    newcontents

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 return array(
 'displayErrorDetails' => true,
 'addContentLengthHeader' => false,
-'log_file'=>'/var/log/wx',
+'log_file'=>'/var/log/wx/wxdev.log',
 'db' => array(
 	'host'   => '127.0.0.1',
 	'user'   => 'wxdev',
